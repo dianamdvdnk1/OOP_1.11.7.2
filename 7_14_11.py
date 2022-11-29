2202eeb0808e3fae7ff922a7dc28e969f1067a7f
dianamdvdnk1 <dianamdvdnk@gmail.com>
2022-11-29 21:39:45 +0300

    hw of 7&14_11

--- 7_14_11.py
+++ 7_14_11.py
@@ -9,26 +9,28 @@ class Triangle:
         self.b = b
         self.c = c
         if not self.is_valid():
-            raise NotValidFigure
+             raise NotValidFigure
 
     def is_valid(self):
         sides = [self.a, self.b, self.c]
         if all([isinstance(side, (int, float)) for side in sides]):
             return all([side > 0 for side in sides]) 
             sortedside = sorted(sides)
-            return sortedside[1] < sortedside[-1] + sortedside[0]
+            return sortedside[-1] < sortedside[0] + sortedside[1]
             
 
     def perimeter(self):
-        return round(self.a + self.b + self.c,3)
+        return self.a + self.b + self.c
 
-    def squrt(self):
+    def square(self):
         half_perimeter = (self.a + self.b + self.c) / 2
-        return round(sqrt(half_perimeter * (half_perimeter - self.a) * (half_perimeter - self.b) * (half_perimeter - self.c)),3)
-    
-triangle_itog = Triangle(.2, .2, .2)
+        return sqrt(half_perimeter * (half_perimeter - self.a) * (half_perimeter - self.b) * (half_perimeter - self.c))
+        return round(sqrt(half_perimeter * (half_perimeter - self.a) * (half_perimeter - self.b) * (half_perimeter - self.c)))
+
+triangle_itog = Triangle(8, 7, 13)
+# triangle_itog = Triangle(.8, .7, .13) #dz za
 print(triangle_itog.perimeter())
-print(triangle_itog.squrt())
+print(triangle_itog.square())
 
 class Circle:
     def __init__(self, radius):
@@ -41,12 +43,12 @@ class Circle:
             return self.radius > 0
     
     def dlina(self):
-        return round(self.radius **2 * pi, 3)
+        return round(self.radius **2 * pi, 1)
 
     def ploshad(self):
-        return round(2 * pi * self.radius, 3)
+        return round(2 * pi * self.radius, 1)
 
-circle_itog = Circle(3)
+circle_itog = Circle(4)
 print(circle_itog.dlina())
 print(circle_itog.ploshad())
 
